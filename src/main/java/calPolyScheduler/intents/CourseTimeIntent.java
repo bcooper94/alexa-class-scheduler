eabf72621f023520d21d21f47a5b4d5bcf3cf8ce
Brandon <bcoopercs@live.com>
2016-11-05 19:10:45 -0700

    Added CourseTime intent

--- src/main/java/calPolyScheduler/intents/CourseTimeIntent.java
+++ src/main/java/calPolyScheduler/intents/CourseTimeIntent.java
@@ -1,7 +1,19 @@
 package calPolyScheduler.intents;
 
+import com.amazon.speech.slu.Intent;
+import com.amazon.speech.speechlet.Session;
+import com.amazon.speech.speechlet.SpeechletResponse;
+
 /**
  * Created by brandon on 11/5/16.
  */
-public class CourseTimeIntent {
+public class CourseTimeIntent extends SchedulerIntent {
+    public CourseTimeIntent(Intent intent, Session session) {
+        super(intent, session);
+    }
+
+    @Override
+    public SpeechletResponse createResponse() {
+        String department = 
+    }
 }
